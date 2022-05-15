eba216bf048f715583025a97697df4f70a053c90
nishanbe <Msjammsjam1!>
2022-05-15 22:34:24 +1000

    Initial Commit

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -14,7 +14,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 	<head>
-
+    <h1>This is a custom</h1>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
