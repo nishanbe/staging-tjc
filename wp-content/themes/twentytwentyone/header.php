d20d26dc23b4c267695ac5766d1a66ec04165bd9
nishanbe <79426795+nishanbe@users.noreply.github.com>
2022-05-16 18:36:22 +1000

    Update header.php

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -24,7 +24,7 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
-
+	<h1>Testing </h1>
 	<?php get_template_part( 'template-parts/header/site-header' ); ?>
 
 	<div id="content" class="site-content">
