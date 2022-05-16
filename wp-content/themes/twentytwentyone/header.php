40ea11d12aefef7e8a930f8e49cc2dbd7ef65446
nishanbe <Msjammsjam1!>
2022-05-16 23:33:12 +1000

    Initial Commit

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -28,6 +28,6 @@
 	<?php get_template_part( 'template-parts/header/site-header' ); ?>
 
 	<div id="content" class="site-content">
-        <h1>Testing - VVV</h1>
+        <h1>Testing - DDD</h1>
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
